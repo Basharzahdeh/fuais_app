d4c1eb7728021e28479fc7690f04cae8ef8389cd
ahmadalbasha77 <136097110+ahmadalbasha77@users.noreply.github.com>
2023-08-16 13:37:23 +0300

    add task screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fuais_app/ui/main/home_screen.dart';
 
+import 'ui/tasks/tasks_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: TasksScreen(),
     );
   }
 }
